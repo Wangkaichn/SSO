c5965a03e07a8c292a39d09c710dd23ac5b9aa2b
wangkai <1137120948@qq.com>
2020-09-13 13:30:07 +0800

    mysql connectPool

--- utils/database/mysql/index.js
+++ utils/database/mysql/index.js
@@ -1,60 +1,37 @@
-const { createConnection } = require('mysql')
+const mysql = require('mysql');
 
-const tableName = 't_user'
-const dbConnection = createConnection({
+const pool  = mysql.createPool({
   host: "123.56.143.183",
   user: "root",
   password: "12312300",
   database: "sso",
   port: "33066",
 })
-
-const _connect = () => {
-  return new Promise((resolve, reject) => {
-    dbConnection.connect((err) => {
-      if (err) {
-        reject(err)
-      }
-      console.log("Mysql 连接成功")
-      resolve()
-    })
-  })
-}
-const _end = () => {
-  return new Promise((resolve, reject) => {
-    try {
-      dbConnection.end()
-      console.log("Mysql 连接断开")
-      resolve()
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
-
 const _query = async (sql) => {
-  await _connect()
-  const res =  new Promise((resolve, reject) => {
-    dbConnection.query(sql, (err, res) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(res)
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function(error, connection) {
+      if (error) {
+        console.warn('Mysql 连接失败')
+        reject(error)
       }
+      connection.query(sql, async (error, results) => {
+        resolve(results)
+        connection.release()
+        if (error) {
+          console.warn(error)
+          reject(error)
+        }
+      })
     })
   })
-  await _end()
-  return res
 }
 
-
+const tableName = 't_user'
 const test = async () => {
   const sql = 'SELECT * FROM ' + tableName
   const res = await _query(sql)
   console.info('res: ', res)
-  _stop()
 }
-
 const checkNickNameIsExisted = async (nickName) => {
   const sql = `SELECT id FROM ${tableName} WHERE nick_name = '${nickName}'` 
   const res = await _query(sql)
